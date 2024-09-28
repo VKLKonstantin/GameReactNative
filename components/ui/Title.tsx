aab85bebf192c0a8184c2a6d3d2dc108811143fc
Konstantin <kos8533@yandex.ru>
2024-09-28 23:51:13 +0300

    Add Platform

--- components/ui/Title.tsx
+++ components/ui/Title.tsx
@@ -1,4 +1,4 @@
-import { Text, StyleSheet } from 'react-native'
+import { Text, StyleSheet, Platform } from 'react-native'
 import { themeColors } from '../../constants/colors';
 
 type TitleProps = {
@@ -6,9 +6,9 @@ type TitleProps = {
 }
 export const Title: React.FC<TitleProps> = ({ text }) => {
   return (
-        <Text style={styles.title}>
-          {text}
-        </Text>
+    <Text style={styles.title}>
+      {text}
+    </Text>
   )
 }
 
@@ -16,8 +16,8 @@ const styles = StyleSheet.create({
   title: {
     color: themeColors.accent500,
     fontWeight: "bold",
-    fontSize: 18, 
-    borderWidth: 2,
+    fontSize: 18,
+    borderWidth: Platform.OS === 'android' ? 5 : 0,
     padding: 5,
     borderColor: themeColors.accent500,
   }
